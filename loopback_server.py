4a0fe1d7148069fdc0f233767d446756fa3f4a00
zota <zota@example.com>
2025-05-10 23:34:20 +0900

    add: halt timeout

--- loopback_server.py
+++ loopback_server.py
@@ -1,5 +1,6 @@
 from udp_sock import ReceiverPort
 from udp_sock import SenderPort
+import socket
 import threading
 import time
 
@@ -9,27 +10,30 @@ class Server:
     def __init__(self):
         self.sender = SenderPort(port=4002, dest_ip="127.0.0.1")
         self.receiver = ReceiverPort(port=4001)
+        self.receiver.sock.settimeout(1.0)
         self.alive = True
         self.th1 = threading.Thread(target=self.receive_thread,daemon=True)
         self.th1.start()
 
     def receive_thread(self):
+        data = None
         while self.alive:
-            data = self.receiver.recv()
-            self.sender.send(data)
+            try:
+                data = self.receiver.recv()
+            except socket.timeout:
+                pass
+            if data:
+                self.sender.send(data)
 
     def halt(self):
-        print("Server halt.")
         self.alive = False
         if self.th1:
-            print("f1")
             self.th1.join()
         if self.sender:
-            print("f2")
             self.sender.close()
         if self.receiver:
             self.receiver.close()
-        print("finish")
+        print("Server halt.")
 
 
 s = Server()
